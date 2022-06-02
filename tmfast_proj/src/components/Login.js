d7dd3ffcd650d5e18945a4fed123d68ee5831698
macoki07 <matthew.ng2001@gmail.com>
2022-06-02 21:11:49 +0800

    loginui2

--- tmfast_proj/src/components/Login.js
+++ tmfast_proj/src/components/Login.js
@@ -4,23 +4,27 @@ import Form from "react-bootstrap/Form";
 
 export default function Login() {
     return (
-      <div className="Login">
-      <Form>
-        <Form.Group size="lg" controlId="username">
-          <Form.Control
-            type="email"
-            placeholder="Username"
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Control
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Group>
-        <button type="submit">Submit</button>
-        
-      </Form>
+      <div className="content">
+        <div className="white">
+        </div>
+        <div className="teal">
+          <div className="Login">
+            <form>
+              <div className="input">
+                <input type="text" src="assets/user.png" id="username" name="username" placeholder="Username"/>
+                <input type="text" src="assets/lock.png" id="password" name="password" placeholder="Password"/>
+              </div>
+              <div className="button">
+                <input type="submit" value="LOGIN" />
+              </div>
+              <div className="small">
+                <small><a href="#">Forgot username/password?</a></small>
+                <small>Don't have an account?<a href="#">Register now.</a></small>
+              </div>
+            </form> 
+          </div>
+        </div>
     </div>
+   
     );
   }
